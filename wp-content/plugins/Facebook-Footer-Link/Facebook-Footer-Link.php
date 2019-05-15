ed96b6d1468a0b53f3c58750c7a18a117dc2c2aa
Jordan <jordanashment@gmail.com>
2019-05-15 15:25:06 +0000

    Making Pluging Visible

---
+++ wp-content/plugins/Facebook-Footer-Link/Facebook-Footer-Link.php
@@ -0,0 +1,13 @@
+<?php 
+    /**
+     * Plugin Name: Facebook Footer Link
+     * Description: Adds a Facebook profile link to the end of posts
+     * Version: 1.0
+     * Author: Jordan Ashment
+     * 
+     **/
+     
+     //Exit if Accessed Directly
+     if(!definded('ABSPATH')){
+         exit;
+     }
